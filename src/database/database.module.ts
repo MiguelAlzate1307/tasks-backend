ed5e17cbb4b9d0620bd615cfd73300e14a54b3ec
Miguel Alzate <alzatemiguel284@gmail.com>
2024-11-22 19:18:47 -0500

    Added DatabaseModule

---
+++ src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configSer: ConfigService) => ({
+        type: 'postgres',
+        host: configSer.get<string>('config.database.host'),
+        port: configSer.get<number>('config.database.port'),
+        username: configSer.get<string>('config.database.user'),
+        password: configSer.get<string>('config.database.pass'),
+        database: configSer.get<string>('config.database.name'),
+        autoLoadEntities: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {
+  constructor() {
+    console.log('DatabaseModule loaded');
+  }
+}
